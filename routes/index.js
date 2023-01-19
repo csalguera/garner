7ee1822acbc0efc6cf6ec12efbc3a11c87a80e2d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-19 10:29:12 -0500

    reformat the home page to render the most recent game submissions, up to 10

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 import { Router } from 'express'
+import { Game } from '../models/game.js'
+import { Platform } from '../models/platform.js'
 
 const router = Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'garner' })
+  Game.find({})
+  .sort({ createdAt: 'desc' })
+  .limit(10)
+  .populate('platforms')
+  .then(games => {
+    Platform.find({ _id: {$in: games.platforms} })
+    .then(platforms => {
+      res.render('index', {
+        games,
+        platforms,
+        title: 'garner'
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 })
 
 export {
